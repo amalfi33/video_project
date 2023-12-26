e41fb6e47b62ec6803db1a584c530c59c1c8a537
amalfi33 <amalfiamalalena1210@gmail.com>
2023-12-26 19:12:35 +0500

    video8

--- video_app/urls.py
+++ video_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('videos/', views.videos, name = 'videos'),
+    path('videos/<slug:slug>', views.post_detail, name = 'post_detail_url'),
 ]
--- video_app/views.py
+++ video_app/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    featureds = Post.objects.filter(publish = 1).filter(featured = 1)[:4]
+    posts = Post.objects.filter(publish = 1).order_by('-date')[:8]
+    context = ({'posts': posts, 'featureds': featureds})
+    return render(request, 'index.html', context)
+
+def videos(request):
+    posts = Post.objects.filter(publish = 1)
+    return render(request, 'videos.html', {'posts': posts})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__exact = slug)
+    return render(request, 'post_detail.html', {'post':post})
